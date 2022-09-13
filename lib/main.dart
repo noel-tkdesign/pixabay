ea8d8a4a7d91d1d0dcc11740152c681bea102af4
noel-tkdesign <79149908+noel-tkdesign@users.noreply.github.com>
2022-09-14 00:00:58 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: PixabayPage(),
+    );
+  }
+}
+
+class PixabayPage extends StatefulWidget {
+  const PixabayPage({Key? key});
+
+  @override
+  State<PixabayPage> createState() => _PixabayPageState();
+}
+
+class _PixabayPageState extends State<PixabayPage> {
+  //はじめは空のlistを入れておく
+  List hits = [];
+
+  Future<void> fetchImages(String text) async {
+    Response response = await Dio().get(
+        'https://pixabay.com/api/?key=29855170-d75f46811c803d124c4a86632&q=$text+flowers&image_type=photo&pretty=true');
+    hits = response.data['hits'];
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchImages('花');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          initialValue: '花',
+          decoration: InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
+          ),
+          onFieldSubmitted: (text) {
+            fetchImages(text);
+          },
+        ),
+      ),
+      body: GridView.builder(
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          itemCount: hits.length,
+          itemBuilder: (context, index) {
+            Map<String, dynamic> hit = hits[index];
+            return InkWell(
+              onTap: () async {
+                //1. URL から画像をダウンロード
+                Response response = await Dio().get(
+                  hit['webformatURL'],
+                  options: Options(responseType: ResponseType.bytes),
+                );
+
+                // 2. ダウンロードしたデータをファイルに保存
+                Directory dir = await getTemporaryDirectory();
+                File file = await File('${dir.path}/image.png')
+                    .writeAsBytes(response.data);
+
+                //3. shareパッケージを呼び出し共有
+                Share.shareFiles([file.path]);
+              },
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.network(
+                    hit['previewURL'],
+                    fit: BoxFit.cover,
+                  ),
+                  Align(
+                      alignment: Alignment.bottomRight,
+                      child: Container(
+                          color: Colors.white,
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(
+                                Icons.thumb_up_off_alt_outlined,
+                                size: 14,
+                              ),
+                              Text('${hit['likes']}'),
+                            ],
+                          ))),
+                ],
+              ),
+            );
+          }),
+    );
+  }
+}
